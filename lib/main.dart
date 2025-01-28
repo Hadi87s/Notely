8d4f301611dbe7c380ee42e0b83a76a68c928b50
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2025-01-28 23:05:39 +0200

    calling FirebaseAuth

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -72,9 +58,22 @@ class _HomePageState extends State<HomePage> {
         ),
         body: Column(
           children: [
-            TextField(),
-            TextField(),
-            TextButton(onPressed: () async {}, child: const Text('Register')),
+            TextField(
+              controller: email,
+              decoration: InputDecoration(hintText: "Your email"),
+            ), // this is how we assign our controller to the text field given.
+            TextField(
+              controller: password,
+              decoration: InputDecoration(hintText: "Your password"),
+            ), // this is how we assign our controller to the text field given.
+            TextButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                      email: email.text, password: password.text);
+                  // created an instance of firebase and give it the username and the password written by the user (the value we got from the textControllers)
+                  // and since this FirebaseAuth returns a future we need to await of it for it to really perform the work we want
+                },
+                child: const Text('Register')),
           ],
         ));
   }
